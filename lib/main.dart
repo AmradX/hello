7340a579c98e076ea536d896edc3d7a95e786c06
Adi Darma N <nurdaya22@gmail.com>
2022-02-09 11:10:09 +0700

    +X

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
+          title: Text("Aplikasi Hello WorldX"),
         ),
         body: Text("Hello World"),
       ),
